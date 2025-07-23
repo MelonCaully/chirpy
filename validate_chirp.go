9ebef50d05088699cdf2b25576d7b0e8696495ce
Meloncaully <maloneingham@gmail.com>
2025-07-23 16:54:39 -0400

    added bad word check handler

--- validate_chirp.go
+++ validate_chirp.go
@@ -3,40 +3,52 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
-type parameters struct {
-	Body  string `json:"body"`
-	Error string `json:"error"`
-	Valid bool   `json:"valid"`
-}
-
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type returnVals struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
 	err := decoder.Decode(&params)
 
-	w.Header().Set("Content-Type", "application/json")
-
 	if err != nil {
-		params.Error = "Something went wrong"
-		w.WriteHeader(500)
-		resp, _ := json.Marshal(params)
-		w.Write(resp)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
 		return
 	}
 
 	if len(params.Body) > 140 {
-		params.Error = "Chirp is too long"
-		w.WriteHeader(400)
-		resp, _ := json.Marshal(params)
-		w.Write(resp)
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
 		return
 	}
 
-	params.Valid = true
-	w.WriteHeader(200)
-	resp, _ := json.Marshal(params)
-	w.Write(resp)
+	badWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
 
+	cleaned := getCleanedBody(params.Body, badWords)
+
+	respondWithJSON(w, http.StatusOK, returnVals{
+		CleanedBody: cleaned,
+	})
+}
+
+func getCleanedBody(body string, badWords map[string]struct{}) string {
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		if _, ok := badWords[strings.ToLower(word)]; ok {
+			words[i] = "****"
+		}
+	}
+	cleaned := strings.Join(words, " ")
+	return cleaned
 }
